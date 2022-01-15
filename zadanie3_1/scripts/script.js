1b00fd130638c117562d073aba3f2a160e904220
Krystian Bendinger <krystian.bendinger@gmail.com>
2022-01-15 13:40:08 +0100

    changed structure

--- zadanie3_1/scripts/script.js
+++ zadanie3_1/scripts/script.js
@@ -10,39 +10,44 @@ const getRandomColor = () => {
   return color;
 }
 
+let lastId = 0;
+
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+const canvasWidth = parseFloat(getComputedStyle(canvas).width);
+const canvasHeight = parseFloat(getComputedStyle(canvas).height);
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+ctx.canvas.width = canvasWidth;
+ctx.canvas.height = canvasHeight;
+
+let lastX = 0;
+let lastY = 0;
+let elementDragged = null;
+const blockedX = [];
+const blockedY = [];
+
 buttonGenerate.addEventListener("click", () => {
   const color = getRandomColor();
-
-  const canvas = document.querySelector("#canvas");
-  const ctx = canvas.getContext("2d");
-
-  const canvasWidth = parseFloat(getComputedStyle(canvas).width);
-  const canvasHeight = parseFloat(getComputedStyle(canvas).height);
-  const canvasOffsetX = canvas.offsetLeft;
-  const canvasOffsetY = canvas.offsetTop;
-
-  ctx.canvas.width = canvasWidth;
-  ctx.canvas.height = canvasHeight;
-
-  let isDragging = false;
-  let lastX = 0;
-  let lastY = 0;
-
   const makeBlock = (x, y, width, height, fill) => {
     const block = {
+      id: lastId,
       x: x,
       y: y,
       width: width,
       height: height,
       fill: fill
     }
+    lastId = lastId + 1;
     blocks.push(block);
   }
 
   const drawAllBlock = () => {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     for (let i = 0; i < blocks.length; i++) {
-      let block = blocks[i]
+      let block = blocks[i];
       drawBlock(block);
       ctx.fillStyle = block.fill;
       ctx.fill();
@@ -61,40 +66,51 @@ buttonGenerate.addEventListener("click", () => {
     mouseX = parseInt(e.clientX - canvasOffsetX);
     mouseY = parseInt(e.clientY - canvasOffsetY);
 
+    elementDragged = blocks.find(block => block.x < mouseX && mouseX < block.x + block.width && block.y < mouseY && mouseY < block.y + block.height)
+    if(elementDragged) {
+      elementDragged.xClicked = mouseX - elementDragged.x
+      elementDragged.yClicked = mouseY - elementDragged.y
+    }
     lastX = mouseX;
     lastY = mouseY;
-    isDragging = true;
   }
 
   const customMouseUp = e => {
     mouseX = parseInt(e.clientX - canvasOffsetX);
     mouseY = parseInt(e.clientY - canvasOffsetY);
 
-    isDragging = false;
+    elementDragged = null;
   }
 
   const customMouseMove = e => {
-    if (!isDragging) {
+    if (!elementDragged) {
         return;
     }
 
+    let collision = false;
     mouseX = parseInt(e.clientX - canvasOffsetX);
     mouseY = parseInt(e.clientY - canvasOffsetY);
-    for (let i = 0; i < blocks.length; i++) {
-        let block = blocks[i];
-        drawBlock(block);
-        if (ctx.isPointInPath(lastX, lastY)) {
-          block.x += (mouseX - lastX);
-          block.y += (mouseY - lastY);
-        }
+
+    drawBlock(elementDragged);
+
+    if (ctx.isPointInPath(lastX, lastY)) {
+      collision = blocks.find(block => block !== elementDragged
+          && block.x <= elementDragged.x + elementDragged.width && elementDragged.x <= block.x + block.width
+          && block.y <= elementDragged.y + elementDragged.height && elementDragged.y <= block.y + block.height) ? true : false;
+
+      if(!collision) {
+        elementDragged.x += (mouseX - lastX);
+        elementDragged.y += (mouseY - lastY);
+      }
     }
+
     lastX = mouseX;
     lastY = mouseY;
     drawAllBlock();
   }
 
   const initialFunction = () => {
-    makeBlock(30, 30, 25, 25, color);
+    makeBlock(30, 30, 60, 60, color);
     drawAllBlock();
   }
 
